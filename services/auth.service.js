e341344904ad68f8dccdda3b2a8c419701315729
samnkrki <samn_krki@live.com>
2023-01-13 10:10:02 +0545

    scripts updated
    files created for auth

---
+++ services/auth.service.js
@@ -0,0 +1,35 @@
+/**
+ * service to create new user
+ * @param {*} userDetails 
+ */
+async function signup(userDetails) {
+    try {
+        // add user logic
+    } catch (error) {
+        throw error;
+    }
+}
+
+async function signin(userDetails) {
+    try {
+        
+    } catch (error) {
+        throw error;
+    }
+}
+
+async function forgotPassword(userDetails) {
+    try {
+        
+    } catch (error) {
+        throw error;
+    }
+}
+
+async function changePassword(userDetails) {
+    try {
+        
+    } catch (error) {
+        throw error;
+    }
+}
---
+++ services/user.service.js
@@ -0,0 +1,17 @@
+async function findOne() {
+    try {
+        // find one user and return it
+    } catch (error) {
+        throw error;
+    }
+}
+
+async function findAll() {
+    try {
+        
+    } catch (error) {
+        throw error;
+    }
+}
+
+module.exports = {findOne, findAll};
