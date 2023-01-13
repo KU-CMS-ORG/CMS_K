b48dd10f67a3e5ef03c2a6140ba98f4c001bd07b
samnkrki <samn_krki@live.com>
2023-01-13 12:36:07 +0545

    route updated

--- routes/index.route.js
+++ routes/index.route.js
@@ -1,7 +1,9 @@
 const authRoute = require("./auth.route");
 const Router = require("@koa/router");
+const foodRoute = require("./food.route");
 
 const router = new Router();
 router.use("/auth", authRoute);
+router.use("/food", foodRoute);
 
 module.exports = router.routes();
