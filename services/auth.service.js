0cd3b69f2a243a8ae450a3870f506dfc75f2e198
samnkrki <samn_krki@live.com>
2023-01-13 12:12:04 +0545

    auth logic updated

--- services/auth.service.js
+++ services/auth.service.js
@@ -1,10 +1,21 @@
+const { PrismaClient, Prisma } = require("@prisma/client");
+
+const prisma = new PrismaClient();
 /**
  * service to create new user
- * @param {*} userDetails 
+ * @param {{}} userDetails 
  */
 async function signup(userDetails) {
     try {
         // add user logic
+        const existingUser = await prisma.tblUser.findUnique({ where: { email: userDetails.email } });
+        if (user) {
+            throw new Error('email already exists');
+        }
+        const newUser = await prisma.tblUser.create({ ...userDetails });
+        // create email with new user's credentials
+        return newUser;
+
     } catch (error) {
         throw error;
     }
@@ -12,7 +23,17 @@ async function signup(userDetails) {
 
 async function signin(userDetails) {
     try {
-        
+        const existingUser = await prisma.tblUser.findUnique({ where: { email: userDetails.email } });
+        if (!existingUser) {
+            throw new Error('user does not exist');
+        }
+        if (existingUser.password == userDetails.password) {
+            //generate jwt and login
+            return existingUser;
+        } else {
+            throw new Error('password does not match');
+        }
+
     } catch (error) {
         throw error;
     }
@@ -20,7 +41,17 @@ async function signin(userDetails) {
 
 async function forgotPassword(userDetails) {
     try {
-        
+        //generate random Password
+        const updatedUser = await prisma.user.update({
+            where: {
+                email: userDetails.email,
+            },
+            data: {
+                password: 'generatedPassword',
+            },
+        })
+        // send email with updated password
+        return updatedUser;
     } catch (error) {
         throw error;
     }
@@ -28,8 +59,25 @@ async function forgotPassword(userDetails) {
 
 async function changePassword(userDetails) {
     try {
-        
+        const existingUser = await prisma.tblUser.findUnique({ where: { email: userDetails.email } });
+
+        if (userDetails.password !== existingUser.password) {
+            throw new Error('old passwords do not match')
+        }
+
+        const updatedUser = await prisma.user.update({
+            where: {
+                email: userDetails.email,
+            },
+            data: {
+                password: userDetails.newPassword,
+            },
+        })
+
+        return updatedUser;
     } catch (error) {
         throw error;
     }
 }
+
+module.exports = { signin, changePassword, signup, forgotPassword };
