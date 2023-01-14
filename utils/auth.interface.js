1cbae15836f7cc34dd329e769734b74d43239726
samnkrki <samn_krki@live.com>
2023-01-14 14:39:39 +0545

    api validator added

--- utils/auth.interface.js
+++ utils/auth.interface.js
@@ -1,26 +1,37 @@
 const Joi = require("joi");
-const schemaValidate = require("./schema.validation");
 
-const signInSchema = Joi.object().keys({
-    email: Joi.string().email().required(),
-    password: Joi.string().required(),
-});
+const signInSchema = {
+    body: Joi.object().keys({
+        email: Joi.string().email().required(),
+        password: Joi.string().required(),
+    }),
+};
 
-const signUpSchema = Joi.object().keys({
-    email: Joi.string().email().required(),
-    fName: Joi.string().required(),
-    mName: Joi.string().required(),
-    lName: Joi.string().required(),
-});
+const signUpSchema = {
+    params: {},
+    query: {},
+    body: Joi.object()
+        .keys({
+            email: Joi.string().email().required(),
+            firstName: Joi.string().required(),
+            middleName: Joi.string().required(),
+            lastName: Joi.string().required(),
+        })
+        .required(),
+};
 
-const forgotPasswordSchema = Joi.object().keys({
-    email: Joi.string().email().required(),
-});
+const forgotPasswordSchema = {
+    body: Joi.object().keys({
+        email: Joi.string().email().required(),
+    }),
+};
 
-const changePasswordSchema = Joi.object().keys({
-    oldPassword: Joi.string().required(),
-    newPassword: Joi.string().required(),
-});
+const changePasswordSchema = {
+    body: Joi.object().keys({
+        oldPassword: Joi.string().required(),
+        newPassword: Joi.string().required(),
+    }),
+};
 
 module.exports = {
     signInSchema,
--- utils/schema.validation.js
+++ utils/schema.validation.js
@@ -5,13 +5,24 @@ const Joi = require("joi");
  * @param {Joi.ObjectSchema<any>} schema
  * @param {{}} data
  */
-function schemaValidate(schema, data) {
-    const { error, value } = schema.validate(data);
-    if (error) {
-        throw error;
-    }
-
-    return value;
+function schemaValidate(schema) {
+    return (ctx, next) => {
+        const params = ctx.request.params;
+        const body = ctx.request.body;
+        const query = ctx.request.query;
+        const validBody = {
+            ...(params && { params }),
+            ...(body && { body }),
+            ...(query && { query }),
+        };
+        const { error, value } = Joi.compile(schema).validate(validBody);
+        if (error) {
+            console.log(error);
+            ctx.throw(400, error.details[0].message);
+            // throw new Error(error.details[0].message);
+        }
+        return next();
+    };
 }
 
 module.exports = schemaValidate;
