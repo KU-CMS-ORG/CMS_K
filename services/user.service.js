d90412e271d3c51358bd334472484388aa31b693
samnkrki <samn_krki@live.com>
2023-01-15 09:09:30 +0545

    user module updated

--- services/user.service.js
+++ services/user.service.js
@@ -1,18 +1,58 @@
 const { PrismaClient, Prisma } = require("@prisma/client");
+const prisma = new PrismaClient();
 
-async function findOne() {
+/**
+ *
+ * @param {{id?:String, email?: String}} where
+ * @returns
+ */
+async function findDetail(whereKey) {
     try {
         // find one user and return it
+        return prisma.tblUser.findUnique({
+            where: whereKey,
+        });
     } catch (error) {
         throw error;
     }
 }
 
-async function findAll() {
+/**
+ * find all users in the system
+ * @param {{limit: Number, page: Number, sortBy: String, sortType: String}} options
+ * @param {{email?:String}} filters
+ */
+async function findAll(options, filters) {
     try {
+        return prisma.tblUser.findMany({ take: options.limit });
     } catch (error) {
         throw error;
     }
 }
 
-module.exports = { findOne, findAll };
+/**
+ * deletes user given its id
+ * @param {{id: String}} whereKey
+ * @returns
+ */
+async function deleteUser(whereKey) {
+    try {
+        return prisma.tblUser.delete({ where: { ...whereKey } });
+    } catch (error) {
+        throw error;
+    }
+}
+
+/**
+ * updates user information
+ * @param {{userId: String, firstName?:String, middleName?:String, lastName?:String, email?: String, rollId?:String, phone?:Number, role?: String, faculty?: String, status?: String}} userDetails
+ */
+async function updateUser(userDetails) {
+    try {
+        return prisma.tblUser.update({ where: { ...userDetails } });
+    } catch (error) {
+        throw error;
+    }
+}
+
+module.exports = { findDetail, findAll, updateUser, deleteUser };
