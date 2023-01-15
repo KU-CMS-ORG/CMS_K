2e5b1a84750073b5e05547a79addce519c457b5f
samnkrki <samn_krki@live.com>
2023-01-15 09:39:16 +0545

    app constants added

---
+++ constants/app.constants.js
@@ -0,0 +1,5 @@
+const ROLES = ["admin", "super-admin", "user"];
+const PAYMENT_TYPES = ["cash", "esewa"];
+const USER_STATUS = ["active", "closed", "inactive"];
+const FOOD_CATEGORY = ["breakfast", "lunch", "dinner", "beverages", "others"];
+const PAYMENT_STATUS = ["pending", "paid"];
