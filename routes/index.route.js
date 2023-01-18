650064c6c8b4584aaa6a5aca617b7e43776cf17a
samnkrki <samn_krki@live.com>
2023-01-18 09:04:24 +0545

    user route added

--- routes/index.route.js
+++ routes/index.route.js
@@ -1,8 +1,10 @@
 const authRoute = require("./auth.route");
 const Router = require("@koa/router");
 const foodRoute = require("./food.route");
+const userRoute = require("./user.route");
 
 const router = new Router();
 router.use("/auth", authRoute);
+router.use("/user", userRoute);
 router.use("/food", foodRoute);
 module.exports = router.routes();
