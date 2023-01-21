0cd8794d532fd588c2b7d9d9df0e2213efb46c56
samnkrki <samn_krki@live.com>
2023-01-21 19:35:16 +0545

    config routes added

--- routes/index.route.js
+++ routes/index.route.js
@@ -4,6 +4,7 @@ const foodRoute = require("./food.route");
 const userRoute = require("./user.route");
 const menuRoute = require("./menu.route");
 const orderRoutes = require("./order.routes");
+const configRoutes = require("./config.route");
 
 const router = new Router();
 router.use("/auth", authRoute);
@@ -11,4 +12,5 @@ router.use("/user", userRoute);
 router.use("/food", foodRoute);
 router.use("/menu", menuRoute);
 router.use("/order", orderRoutes);
+router.use("/config", configRoutes);
 module.exports = router.routes();
