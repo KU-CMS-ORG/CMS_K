f16ce5cba61fb49f1c3dd1eaf5e6cf0ae8ea3541
Julius <juliusshrestha29@gmail.com>
2023-02-10 14:26:25 +0545

    get total trasaction service

--- services/analytics.service.js
+++ services/analytics.service.js
@@ -2,20 +2,11 @@ const { PrismaClient } = require("@prisma/client");
 
 const prisma = new PrismaClient();
 async function getTotalTransaction(ctx) {
-    const userId = ctx.params.id;
-
-    // Assuming you have a database with a `transactions` table
-    const transactions = await prisma.query(
-        "SELECT * FROM tblTranHistory WHERE userId = ?",
-        [userId]
-    );
-
-    // Sum up the transaction amounts
-    const total = transactions.reduce(
-        (acc, transaction) => acc + transaction.amount,
-        0
-    );
-
-    ctx.body = { total };
+    const total = await prisma.tblTranHistory.aggregate({
+        _sum: {
+            checkoutPrice: true,
+        },
+    });
+    return { totalPrice: total._sum.checkoutPrice };
 }
 module.exports = { getTotalTransaction };
