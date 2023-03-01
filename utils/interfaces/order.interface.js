7faaf142ed225929da2683b8e2d88f0cd1e624e0
Julius <juliusshrestha29@gmail.com>
2023-03-01 22:22:53 +0545

    order sort options added

--- utils/interfaces/order.interface.js
+++ utils/interfaces/order.interface.js
@@ -8,13 +8,40 @@ const orderDetailSchema = {
     query: {},
     body: {},
 };
-
+const allowedSort = [
+    "tranId:asc",
+    "tranId:desc",
+    "user:asc",
+    "user:desc",
+    "userId:asc",
+    "userId:desc",
+    "food:asc",
+    "food:desc",
+    "foodId:asc",
+    "foodId:desc",
+    "tranDate:asc",
+    "tranDate:desc",
+    "quantity:asc",
+    "quantity:desc",
+    "tranStatus:asc",
+    "tranStatus:desc",
+    "createdAt:asc",
+    "createdAt:desc",
+    "updatedAt:asc",
+    "updatedAt:desc",
+    "payment:asc",
+    "payment:desc",
+];
 const ordersListSchema = {
     params: {},
     query: Joi.object().keys({
         limit: Joi.number().required().min(0).required(),
         page: Joi.number().required().min(1).required(),
         search: Joi.string().optional(),
+        sort: Joi.alternatives().try(
+            Joi.array().items(Joi.string().valid(...allowedSort)),
+            Joi.string().valid(...allowedSort)
+        ),
     }),
     body: {},
 };
